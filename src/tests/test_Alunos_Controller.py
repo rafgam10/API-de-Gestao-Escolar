0d834b3959829cb1fc8910b2c863743910c5e9d2
Timoteo <timoteo@hdinfo-ma.com>
2025-11-03 16:19:04 -0300

    main: test Alunos_Controller, feito com sucesso, falta apenas algumas funções para testar.

---
+++ src/tests/test_Alunos_Controller.py
@@ -0,0 +1,14 @@
+from src.models.Alunos import Alunos
+from datetime import date
+
+def test_criar_aluno():
+    aluno_test = Alunos(
+        nome="test",
+        matricula="000-123456",
+        data_nascimento=date(2020, 3, 12),
+        status=1
+    )
+
+    assert aluno_test.nome == "test"
+    assert aluno_test.matricula == "000-123456"
+    assert aluno_test.status == 1
