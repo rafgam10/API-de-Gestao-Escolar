b732551ba6e16390e09e343258401d2719581fd6
Timoteo <timoteo@hdinfo-ma.com>
2025-11-03 16:47:53 -0300

    main: modificações no test_aluno_controller.py

--- src/tests/test_Alunos_Controller.py
+++ src/tests/test_Alunos_Controller.py
@@ -12,10 +12,17 @@ def test_criar_aluno():
     assert aluno_test.nome == "test"
     assert aluno_test.matricula == "000-123456"
     assert aluno_test.status == 1
+    assert aluno_test.data_nascimento == date(2020, 3, 12)
 
 
 def test_lista_alunos():
     pass
 
 def test_busca_aluno_id():
+    pass
+
+def test_atualizar_aluno():
+    pass
+
+def test_excluir_aluno():
     pass
